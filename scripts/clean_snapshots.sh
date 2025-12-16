13aecb8eb693bf868f607b2671b818059f5643cf
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2025-12-16 23:34:13 +0100

    Keep 5 snapshots

--- scripts/clean_snapshots.sh
+++ scripts/clean_snapshots.sh
@@ -2,7 +2,9 @@
 
 source snapshot_fcts.sh
 
-for i in $(seq 3 30)
+TO_KEEP=5
+
+for i in $(seq $TO_KEEP 30)
   do dt=$(date --date="-${i}day" +%F)
      if [ -x $SNAPSHOT_DIR/full/$dt ] || [ -x $SNAPSHOT_DIR/full/$dt ]
         then do_destroy $dt
